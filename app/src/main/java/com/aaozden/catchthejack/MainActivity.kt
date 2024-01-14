341e4e0555e62f49241f409ccf1a7527d1df5546
ahmet <ahmetalparslanozden@gmail.com>
2024-01-14 19:31:22 +0300

    Reverse

--- app/src/main/java/com/aaozden/catchthejack/MainActivity.kt
+++ app/src/main/java/com/aaozden/catchthejack/MainActivity.kt
@@ -2,16 +2,21 @@ package com.aaozden.catchthejack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.renderscript.ScriptGroup.Binding
 import android.view.View
 import android.widget.ImageView
 import com.aaozden.catchthejack.databinding.ActivityMainBinding
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     var score = 0
     var imageArray = ArrayList<ImageView>()
+    var runnable = Runnable{}
+    var handler = Handler(Looper.getMainLooper())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -24,7 +29,9 @@ class MainActivity : AppCompatActivity() {
         for (image in imageArray) {
             image.visibility = View.INVISIBLE
         }
-        imageArray[6].visibility = View.VISIBLE
+        val random = Random()
+        val randomIndex = random.nextInt(9)
+        imageArray[randomIndex].visibility = View.VISIBLE
     }
 
 
